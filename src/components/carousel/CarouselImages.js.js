9096e004c37dbc5d83061da65a16680402a8243e
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2021-08-03 15:44:20 +1000

    centered side-bar

--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -25,16 +25,11 @@ import laptop1 from '../images/laptop1.jpg'
                 <div className={classes.image_container}>
                     <img className={classes.image} src={Pencil} alt="myImage"/>
                     <div  className={classes.h2}> 
-                    <h1> I love building</h1>
-                    <h1>THINGS!!</h1>
-                    <a href="https://github.com/GabrielMcHugh" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject/></a>
+                        <h1> I love building</h1>
+                        <h1>THINGS!!</h1>
+                        <a href="https://github.com/GabrielMcHugh" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject/></a>
                     </div>
                 </div>
-                {/* <div className={classes.image_container}>
-                    <img className={classes.image} src={laptop1} alt="myImage"/>
-                    <div  className={classes.h1}> 
-                    </div>
-                </div> */}
             </Carousel>
           {/* </ScrollAnimation> */}
         </div>
