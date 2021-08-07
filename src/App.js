88d4493e7ad6f0a13b6875fe693633235a3804b2
Gabriel McHugh <gabrielmchugh@gabriels-mbp.home>
2021-08-07 13:35:50 +1000

    Created template files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
-import React,{Component} from 'react';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
- import Sidebar from './components/sidebar/Sidebar';
-import About from './components/about/About';
-import Education from './components/Education/Education';
-import Interest from './components/skills/Interest'
-import CarouselImages from './components/carousel/CarouselImages.js'
 
 
-class App extends Component {
-
-  render(){
+export default function App() {
   return (
-    <Router>
-      <div className="App">
-          <div className="side">
-            <nav className="navbar side navbar-expand-lg navbar-light p-0" >
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{zIndex:'1'}}>
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <Sidebar />
-              </div>
-            </nav>  
-          </div>
-            <div className="main">
-              <div>
-
-                 <CarouselImages />
-              </div>
-                 <About />
-                <Interest />
-                <Education />
-            </div>
-      </div>
-   </Router>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
-  }
 }
-
-export default App;
